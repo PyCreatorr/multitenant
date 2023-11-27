116920e0dc8670a3368b567693aaf3fdb51bd5d0
PyCreatorr <ttptv@yahoo.com>
2023-11-27 11:19:52 +0100

    initial commit

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  root 'static_pages#landing_page'
+  get 'dashboard', to: 'static_pages#dashboard'
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+end
