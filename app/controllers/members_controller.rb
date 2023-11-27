7f63dde6afef941e2ef8be1f8095ffeef9d1abc7
PyCreatorr <ttptv@yahoo.com>
2023-11-27 22:08:50 +0100

    Display tenant members

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -1,2 +1,14 @@
 class MembersController < ApplicationController
+    before_action :set_current_tenant, only: [:index]
+    def index
+        #@members = Member.find(params[:tenant_id].
+        @members = @current_tenant.members
+    end
+
+    private
+
+    def set_current_tenant
+        @current_tenant = Tenant.find(params[:tenant_id])        
+    end
+
 end
