b4db3c69779ef2fe0e5c7bda1ee563361da765b0
PyCreatorr <ttptv@yahoo.com>
2023-11-28 11:18:20 +0100

    Invite users to be the members of the current tenant & provide validation

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -1,8 +1,29 @@
 class MembersController < ApplicationController
-    before_action :set_current_tenant, only: [:index]
+    before_action :set_current_tenant, only: [:index, :invite]
     def index
         #@members = Member.find(params[:tenant_id].
         @members = @current_tenant.members
+    end
+
+    def invite
+        #binding.break
+        email = params[:email]
+        unless email.blank?  
+            flash[:danger] = "No email is provided"
+            return redirect_to tenant_members_path(@current_tenant)
+        end
+        user = User.find_by(email: email) || User.invite!({email: email}, current_user)
+
+        unless user.valid?  
+            flash[:danger] = "Email invalid"
+            return redirect_to tenant_members_path(@current_tenant)
+        end
+
+        user.members.find_or_create_by(tenant: @current_tenant, roles: {admin:false, editor: true})
+        
+        flash[:success] = "Member with #{email} is invited!"
+        redirect_to tenant_members_path(@current_tenant)
+
     end
 
     private
