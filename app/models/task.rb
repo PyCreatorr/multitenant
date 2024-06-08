34db0c5d28fe0577c200073bc718f197bbe13c98
PyCreatorr <ttptv@yahoo.com>
2024-06-08 14:50:17 +0200

    Correct attachments_size

--- app/models/task.rb
+++ app/models/task.rb
@@ -19,7 +19,7 @@ class Task < ApplicationRecord
   def attachments_size
     
       # debugger
-      if cover_image.blob.byte_size > 1.megabytes
+      if cover_image.attached? && cover_image.blob.byte_size > 1.megabytes
         # flash[:danger] = "One or more files are too large. Please upload files smaller than 1 MB each."
         # return redirect_to root_path
        errors.add(:cover_image, "One or more files are too large. Please upload files smaller than 1 MB each.")
