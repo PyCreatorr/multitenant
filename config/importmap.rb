9c2d3d886dde9ccb4b3921e46812fe87027d39c5
PyCreatorr <ttptv@yahoo.com>
2024-06-08 18:05:36 +0200

    nodejs buildpack

--- config/importmap.rb
+++ config/importmap.rb
@@ -6,10 +6,12 @@ pin "@hotwired/stimulus", to: "https://ga.jspm.io/npm:@hotwired/stimulus@3.2.2/d
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
 pin_all_from "app/javascript/controllers", under: "controllers"
 
+# pin "flowbite", to: "https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.2.0/flowbite.turbo.min.js"
+pin "trix", preload: true
+pin "@rails/actiontext", to: "actiontext.js", preload: true
+
 pin "@fortawesome/fontawesome-free", to: "https://ga.jspm.io/npm:@fortawesome/fontawesome-free@6.1.1/js/all.js"
 pin "sortablejs", to: "https://ga.jspm.io/npm:sortablejs@1.15.1/modular/sortable.esm.js"
 pin "stimulus-use", to: "https://ga.jspm.io/npm:stimulus-use@0.52.1/dist/index.js"
 
-# pin "flowbite", to: "https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.2.0/flowbite.turbo.min.js"
-pin "trix", preload: true
-pin "@rails/actiontext", to: "actiontext.js", preload: true
+
