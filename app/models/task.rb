9b46db84aa13a278d46a1b020dc79990507b9f70
PyCreatorr <ttptv@yahoo.com>
2024-06-08 15:32:51 +0200

    trix error

--- app/models/task.rb
+++ app/models/task.rb
@@ -16,15 +16,15 @@ class Task < ApplicationRecord
 
   private
 
-  def attachments_size
+  # def attachments_size
     
-      # debugger
-      if cover_image.attached? && cover_image.blob.byte_size > 1.megabytes
-        # flash[:danger] = "One or more files are too large. Please upload files smaller than 1 MB each."
-        # return redirect_to root_path
-       errors.add(:cover_image, "One or more files are too large. Please upload files smaller than 1 MB each.")
+  #     # debugger
+  #     if cover_image.attached? && cover_image.blob.byte_size > 1.megabytes
+  #       # flash[:danger] = "One or more files are too large. Please upload files smaller than 1 MB each."
+  #       # return redirect_to root_path
+  #      errors.add(:cover_image, "One or more files are too large. Please upload files smaller than 1 MB each.")
 
-      end
-  end
+  #     end
+  # end
 
 end
