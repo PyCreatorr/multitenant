430fcdc8b3449e037313214bdad3a7caffade502
PyCreatorr <ttptv@yahoo.com>
2024-06-08 17:33:28 +0200

    trix error

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -62,7 +62,7 @@ Rails.application.configure do
   # assets.compile option enables the live compilation through Sprockets
   # https://guides.rubyonrails.org/asset_pipeline.html#live-compilation
   # config.assets.compile = true
-  config.assets.debug = true
+  # config.assets.debug = true
   # config.assets.digest = false
 
   # Raises error for missing translations.
