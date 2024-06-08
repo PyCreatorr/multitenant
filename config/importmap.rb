1ab60cf45edbd2915bafae359199fd77e8d66ed6
PyCreatorr <ttptv@yahoo.com>
2024-06-08 18:09:24 +0200

    package new

--- config/importmap.rb
+++ config/importmap.rb
@@ -7,6 +7,7 @@ pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
 pin_all_from "app/javascript/controllers", under: "controllers"
 
 # pin "flowbite", to: "https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.2.0/flowbite.turbo.min.js"
+
 pin "trix", preload: true
 pin "@rails/actiontext", to: "actiontext.js", preload: true
 
